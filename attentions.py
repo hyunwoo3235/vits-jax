2bfa4acc1134672711927f8a34a2600e1174b76e
hyunwoo3235 <hyunwoo3235@gmail.com>
2022-12-31 16:06:59 +0900

    Mistake about attention

--- attentions.py
+++ attentions.py
@@ -10,7 +10,7 @@ class Encoder(nn.Module):
     n_heads: int
     n_layers: int
     kernel_size: int = 1
-    p_dropout: float = 0.
+    p_dropout: float = 0.0
     window_size: int = 4
     dtype: jnp.dtype = jnp.float32
 
@@ -22,23 +22,27 @@ class Encoder(nn.Module):
         ffn_layers = []
         norm_layers_2 = []
         for i in range(self.n_layers):
-            attn_layers.append(MultiHeadAttention(
-                self.hidden_channels,
-                self.hidden_channels,
-                self.n_heads,
-                self.p_dropout,
-                self.window_size,
-                dtype=self.dtype,
-            ))
+            attn_layers.append(
+                MultiHeadAttention(
+                    self.hidden_channels,
+                    self.hidden_channels,
+                    self.n_heads,
+                    self.p_dropout,
+                    self.window_size,
+                    dtype=self.dtype,
+                )
+            )
             norm_layers_1.append(nn.LayerNorm(self.hidden_channels))
-            ffn_layers.append(FFN(
-                self.hidden_channels,
-                self.hidden_channels,
-                self.filter_channels,
-                self.kernel_size,
-                self.p_dropout,
-                dtype=self.dtype,
-            ))
+            ffn_layers.append(
+                FFN(
+                    self.hidden_channels,
+                    self.hidden_channels,
+                    self.filter_channels,
+                    self.kernel_size,
+                    self.p_dropout,
+                    dtype=self.dtype,
+                )
+            )
             norm_layers_2.append(nn.LayerNorm(self.hidden_channels))
 
         self.attn_layers = attn_layers
@@ -70,7 +74,7 @@ class MultiHeadAttention(nn.Module):
     channels: int
     out_channels: int
     n_heads: int
-    p_dropout: float = 0.
+    p_dropout: float = 0.0
     window_size: bool = None
     heads_share: bool = True
     dtype: jnp.dtype = jnp.float32
@@ -87,12 +91,12 @@ class MultiHeadAttention(nn.Module):
             n_heads_rel = 1 if self.heads_share else self.n_heads
             rel_stddev = self.k_channels ** -0.5
             self.emb_rel_k = self.param(
-                'emb_rel_k',
+                "emb_rel_k",
                 nn.initializers.normal(stddev=rel_stddev),
                 (n_heads_rel, self.window_size * 2 + 1, self.k_channels),
             )
             self.emb_rel_v = self.param(
-                'emb_rel_v',
+                "emb_rel_v",
                 nn.initializers.normal(stddev=rel_stddev),
                 (n_heads_rel, self.window_size * 2 + 1, self.k_channels),
             )
@@ -110,27 +114,35 @@ class MultiHeadAttention(nn.Module):
 
     def attention(self, query, key, value, mask=None, deterministic: bool = True):
         b, t_s, d, t_t = (*key.shape, query.shape[1])
-        query = query.reshape(b, self.n_heads, self.k_channels, t_t)
-        key = key.reshape(b, self.n_heads, self.k_channels, t_s)
-        value = value.reshape(b, self.n_heads, self.k_channels, t_s)
+        query = query.reshape(b, t_t, self.n_heads, self.k_channels).transpose(0, 2, 1, 3)
+        key = key.reshape(b, t_s, self.n_heads, self.k_channels).transpose(0, 2, 1, 3)
+        value = value.reshape(b, t_s, self.n_heads, self.k_channels).transpose(0, 2, 1, 3)
 
-        scores = jnp.matmul(key.transpose(0, 1, 3, 2), query / math.sqrt(self.k_channels))
+        scores = jnp.matmul(
+            query / math.sqrt(self.k_channels), key.transpose(0, 1, 3, 2)
+        )
         if self.window_size is not None:
             key_relative_embeddings = self._get_relative_embeddings(self.emb_rel_k, t_s)
-            rel_logits = self._matmul_with_relative_keys(query / math.sqrt(self.k_channels), key_relative_embeddings)
+            rel_logits = self._matmul_with_relative_keys(
+                query / math.sqrt(self.k_channels), key_relative_embeddings
+            )
             scores_local = self._relative_position_to_absolute_position(rel_logits)
             scores += scores_local
         if mask is not None:
             scores = jnp.where(mask == 0, -1e4, scores)
         p_attn = nn.softmax(scores, axis=-1)
         p_attn = self.drop(p_attn, deterministic=deterministic)
-        output = jnp.matmul(value, p_attn)
+        output = jnp.matmul(p_attn, value)
         if self.window_size is not None:
             relative_weights = self._absolute_position_to_relative_position(p_attn)
-            value_relative_embeddings = self._get_relative_embeddings(self.emb_rel_v, t_s)
-            output += self._matmul_with_relative_values(relative_weights, value_relative_embeddings)
+            value_relative_embeddings = self._get_relative_embeddings(
+                self.emb_rel_v, t_s
+            )
+            output += self._matmul_with_relative_values(
+                relative_weights, value_relative_embeddings
+            )
 
-        output = output.reshape(b, t_t, d)
+        output = output.transpose(0, 2, 1, 3).reshape(b, t_t, d)
         return output
 
     def _matmul_with_relative_values(self, x, y):
@@ -140,7 +152,7 @@ class MultiHeadAttention(nn.Module):
         ret: [b, h, l, d]
         """
         ret = jnp.matmul(x, jnp.expand_dims(y, axis=0))
-        return ret.transpose(0, 1, 3, 2)
+        return ret
 
     def _matmul_with_relative_keys(self, x, y):
         """
@@ -148,7 +160,7 @@ class MultiHeadAttention(nn.Module):
         y: [h or 1, m, d]
         ret: [b, h, l, d]
         """
-        ret = jnp.matmul(x.transpose(0, 1, 3, 2), y.transpose(0, 2, 1))
+        ret = jnp.matmul(x, jnp.expand_dims(y.transpose(0, 2, 1), axis=0))
         return ret
 
     def _get_relative_embeddings(self, relative_embeddings, length):
@@ -163,7 +175,9 @@ class MultiHeadAttention(nn.Module):
             )
         else:
             padded_relative_embeddings = relative_embeddings
-        used_relative_embeddings = padded_relative_embeddings[:, slice_start_position:slice_end_position]
+        used_relative_embeddings = padded_relative_embeddings[
+                                   :, slice_start_position:slice_end_position
+                                   ]
         return used_relative_embeddings
 
     def _relative_position_to_absolute_position(self, x):
@@ -177,7 +191,9 @@ class MultiHeadAttention(nn.Module):
         x_flat = x.reshape(batch, heads, length * 2 * length)
         x_flat = jnp.pad(x_flat, ((0, 0), (0, 0), (0, length - 1)))
 
-        x_final = x_flat.reshape(batch, heads, length + 1, 2 * length - 1)[..., :length, length - 1:]
+        x_final = x_flat.reshape(batch, heads, length + 1, 2 * length - 1)[
+                  ..., :length, length - 1:
+                  ]
         return x_final
 
     def _absolute_position_to_relative_position(self, x):
@@ -198,12 +214,14 @@ class FFN(nn.Module):
     out_channels: int
     filter_channels: int
     kernel_size: int = 1
-    p_dropout: float = 0.
+    p_dropout: float = 0.0
     activation: str = None
     dtype: jnp.dtype = jnp.float32
 
     def setup(self):
-        self.conv1 = nn.Conv(self.filter_channels, (self.kernel_size,), dtype=self.dtype)
+        self.conv1 = nn.Conv(
+            self.filter_channels, (self.kernel_size,), dtype=self.dtype
+        )
         self.conv2 = nn.Conv(self.out_channels, (self.kernel_size,), dtype=self.dtype)
         self.drop = nn.Dropout(rate=self.p_dropout)
 
@@ -223,4 +241,4 @@ class FFN(nn.Module):
         pad_l = (self.kernel_size - 1) // 2
         pad_r = self.kernel_size // 2
         padding = ((0, 0), (0, 0), (pad_l, pad_r))
-        return jnp.pad(x, padding, mode='constant', constant_values=0)
+        return jnp.pad(x, padding, mode="constant", constant_values=0)
